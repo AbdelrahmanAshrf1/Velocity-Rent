15a11bf6e106e14e1e10103db9bec4cc90c0c47c
Abdelrahman <Abdelrahman.ashrf1@gmail.com>
2026-01-23 02:15:09 +0200

    Change map style

--- VelocityRent_UI/Map/Services/MapboxService.cs
+++ VelocityRent_UI/Map/Services/MapboxService.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Web.WebView2.WinForms;
+﻿using Microsoft.Web.WebView2.Core;
+using Microsoft.Web.WebView2.WinForms;
 using System;
 using System.Collections.Generic;
 using System.Globalization;
@@ -13,19 +14,26 @@ namespace Velocity_Rent.Map.Services
     public class MapboxService : IMapService
     {
         private WebView2 _webView;
+        private bool _isInitialized = false;
         public bool IsMapReady { get; private set; }
 
         public async Task InitializeAsync(WebView2 webView)
         {
+            if(_isInitialized) return;  
+
+            _isInitialized = true;
             _webView = webView;
+
             await _webView.EnsureCoreWebView2Async();
-
-            _webView.WebMessageReceived += (e, a) => { if (a.TryGetWebMessageAsString() == "MAP_READY") IsMapReady = true; };
-
-            string mapPath = Path.Combine(System.Windows.Forms.Application.StartupPath, "map", "map.html");
+            _webView.WebMessageReceived += OnMessageRecived;
+            await webView.CoreWebView2.Profile.ClearBrowsingDataAsync(Microsoft.Web.WebView2.Core.CoreWebView2BrowsingDataKinds.AllSite);
+            string mapPath = Path.Combine(System.Windows.Forms.Application.StartupPath, @"map\Services\WebFiles", "map.html");
             _webView.Source = new Uri(mapPath);
         }
-
+        private void OnMessageRecived(object sender , CoreWebView2WebMessageReceivedEventArgs e )
+        {
+            if (e.TryGetWebMessageAsString() == "MAP_READY") IsMapReady = true;
+        }
         public async Task MoveToAsync(double lat,double lon,int zoom = 14)
         {
             if (!IsMapReady) return;
---
+++ VelocityRent_UI/Map/Services/WebFiles/map.js
@@ -0,0 +1,50 @@
+﻿mapboxgl.accessToken = 'pk.eyJ1IjoiYWJkZWxyYWhtYW5hc2hyZjEiLCJhIjoiY21rZHF2NTdwMGJhNjNjczd1NThoMXpqdiJ9.h62oi_6DQGS6PogZkTVp2g';
+
+window.map = null;
+window.mapReady = false;
+
+window.initMap = function () {
+
+    window.map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/abdelrahmanashrf1/cmkpbzphk002k01r06vlee5bl',
+        center: [31.2357, 30.0444],
+        zoom: 13,
+        pitch: 45,
+        bearing: -15,
+        antialias: true 
+    });
+
+    map.on('load', () => {
+
+        window.mapReady = true;
+
+        if (window.chrome?.webview)
+        {
+            chrome.webview.postMessage("MAP_READY");
+        }
+
+        map.setConfigProperty('basemap', 'show3dBuildings', true);
+
+        map.easeTo({
+            zoom: 15,
+            pitch: 60,
+            bearing: -20,
+            duration: 2000
+        });
+    });
+};
+
+window.moveMap = function (lat, lon, zoom = 15) {
+    if (!window.mapReady) return;
+
+    map.flyTo({
+        center: [lon, lat],
+        zoom: zoom,
+        speed: 1.2,
+        curve: 1.4,
+        essential: true
+    });
+};
+
+window.onload = window.initMap;
