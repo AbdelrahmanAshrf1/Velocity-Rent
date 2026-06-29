5d345b1e94d346df218fecca9bb5f483f670a88b
Abdelrahman <Abdelrahman.ashrf1@gmail.com>
2026-06-29 19:46:01 +0300

    feat(auth): add role-based permission management

---
+++ VelocityRent.Entities/Enums/enUserPermission.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace VelocityRent.Entities.Enums
+{
+    [Flags]
+    public enum enUserPermission
+    {
+        None = 0,
+        ManageUsers = 1,
+        ManageCars = 2,
+        ManageCustomers = 4,
+        ManageRentals = 8,
+        ViewReports = 16
+    };
+}
---
+++ VelocityRent_DLL/Authorization/RolePermissionManager.cs
@@ -0,0 +1,39 @@
+﻿
+using VelocityRent.Entities.Enums;
+
+namespace VelocityRent_DLL.Authorization
+{
+    public static class RolePermissionManager
+    {
+        public static enUserPermission GetPermission(enUserRole role)
+        {
+            switch(role)
+            {
+                case enUserRole.Admin:
+                    return enUserPermission.ManageUsers
+                         | enUserPermission.ManageCars
+                         | enUserPermission.ManageCustomers
+                         | enUserPermission.ManageRentals
+                         | enUserPermission.ViewReports;
+
+                case enUserRole.Employee:
+                    return enUserPermission.ManageCars
+                         | enUserPermission.ManageCustomers
+                         | enUserPermission.ManageRentals;
+                
+                case enUserRole.Manager:
+                    return enUserPermission.ManageCars
+                         | enUserPermission.ViewReports;
+                
+                default:
+                    return enUserPermission.None;
+            }
+        }
+
+        public static bool HasPermission(enUserRole role , enUserPermission permission)
+        {
+            var permissions = GetPermission(role);
+            return (permissions & permission) == permission;
+        }
+    }
+}
